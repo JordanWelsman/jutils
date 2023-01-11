bd53c96f7e28a28ccfb0659f16f6107c11fed19a
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-11 00:26:32 -0800

    docs: Replaced functions with classes.

--- jutl/formatting/__init__.py
+++ jutl/formatting/__init__.py
@@ -1,3 +1,3 @@
-# These imports raise functions from file-level to submodule-level.
+# These imports raise classes from file-level to submodule-level.
 from .color import TextColor, BackgroundColor
 from .text import Typography, Reset
