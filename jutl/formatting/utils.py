69b693855d64e428986402077867c4e40702d9fa
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-11 16:51:20 -0800

    feat(formatting): Added error handling to apply() function.

--- jutl/formatting/utils.py
+++ jutl/formatting/utils.py
@@ -7,11 +7,11 @@ def apply(text: str, text_color: str = None, background_color: str = None, typog
     formatting = ""
 
     if text_color is not None:
-        formatting += Text.colors[text_color.upper()]
+        formatting += Text.colors[text_color.upper()] if text_color.upper() in Text.colors else ''
     if background_color is not None:
-        formatting += Background.colors[background_color.upper()]
+        formatting += Background.colors[background_color.upper()] if background_color.upper() in Background.colors else ''
     if typography is not None:
-        formatting += Typography.types[typography.upper()]
+        formatting += Typography.types[typography.upper()] if typography.upper() in Typography.types else ''
     return formatting + text + Reset.types['ALL']
 
 def test():
