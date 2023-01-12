dd05ef84312bcef38f7c462a3f7230324e0d040d
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-12 00:12:12 -0800

    refac: Renamed hello_world.py to utils.py to avoid function/filename clash.

--- jutl/utilities/__init__.py
+++ jutl/utilities/__init__.py
@@ -1,8 +1,8 @@
-# Import hello_world.py so module
-# classes and functions are usable at
-# 'from jutl import formatting' level.
-from .hello_world import *
+# Import utils.py so module
+# functions are usable at
+# 'from jutl import utilities' level.
+from .utils import *
 
 # Only show functions specified in
 # submodule files to the outside world.
-__all__ = hello_world.__all__
+__all__ = utils.__all__
