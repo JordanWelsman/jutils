0d01653ffb3ae79f69621e91bba28bb165b5fec3
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-01-11 18:14:04 -0800

    docs: Clarified import functions and __all__ calls.

--- jutl/formatting/__init__.py
+++ jutl/formatting/__init__.py
@@ -1,8 +1,10 @@
-# These imports raise classes from file-level to submodule-level.
-# from .color import TextColor, BackgroundColor
-# from .text import Typography, Reset
-
+# Import color.py, text.py, and utils.py so
+# module classes and functions are usable at
+# 'from jutl import formatting' level.
 from .color import *
 from .text import *
 from .utils import *
+
+# Only show functions specified in
+# submodule files to the outside world.
 __all__ = color.__all__, text.__all__, utils.__all__
