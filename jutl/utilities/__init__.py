b9eadbdffdfaf18388675465bbd1bf9a12990c46
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-02-12 00:39:59 -0800

    refactor: Renamed 'utilities/utils' to 'utilities/helloworld'.

--- jutl/utilities/__init__.py
+++ jutl/utilities/__init__.py
@@ -1,8 +1,8 @@
 # Import utils.py so module
 # functions are usable at
 # 'from jutl import utilities' level.
-from .utils import *
+from .helloworld import *
 
 # Only show functions specified in
 # submodule files to the outside world.
-__all__ = utils.__all__
+__all__ = helloworld.__all__
