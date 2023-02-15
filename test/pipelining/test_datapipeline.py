ffb5daf3985391481ef34128439e1011c8075912
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-02-15 01:58:12 -0800

    test: Started writing unit tests for pipelines.

---
+++ test/pipelining/test_datapipeline.py
@@ -0,0 +1,4 @@
+# jutils/test/pipelining/test_datapipeline.py
+from jutl.pipelining import DataPipeline
+
+test_name: str = "Test name"
---
+++ test/pipelining/test_instructionpipeline.py
@@ -0,0 +1,4 @@
+# jutils/test/pipelining/test_instructionpipeline.py
+from jutl.pipelining import InstructionPipeline
+
+test_name: str = "Test name"
