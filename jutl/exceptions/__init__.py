7e5d217a22c4fd023bb85acd422dbf2b18938295
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-02-18 03:10:11 -0800

    feat: Added more exceptions.

--- jutl/exceptions/__init__.py
+++ jutl/exceptions/__init__.py
@@ -2,8 +2,9 @@
 # classes and functions are usable at
 # 'from jutl.exceptions import _' level.
 from .emptypipeline import *
+from .invalidformatting import *
 from .missinginput import *
 
 # Only show functions specified in
 # submodule files to the outside world.
-__all__ = emptypipeline.__all__, missinginput.__all__
+__all__ = emptypipeline.__all__, invalidformatting.__all__, missinginput.__all__
