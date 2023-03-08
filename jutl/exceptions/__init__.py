175777ca5db67d7eac363178ba079de77c2e285b
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-03-08 01:15:22 -0600

    build: Added JutilsException to exceptions/__init__.py.

--- jutl/exceptions/__init__.py
+++ jutl/exceptions/__init__.py
@@ -7,8 +7,9 @@ from .emptystack import *
 from .fullqueue import *
 from .fullstack import *
 from .invalidformatting import *
+from .jutlexception import *
 from .missinginput import *
 
 # Only show functions specified in
 # submodule files to the outside world.
-__all__ = emptypipeline.__all__, emptyqueue.__all__, emptystack.__all__, fullqueue.__all__, fullstack.__all__, invalidformatting.__all__, missinginput.__all__
+__all__ = emptypipeline.__all__, emptyqueue.__all__, emptystack.__all__, fullqueue.__all__, fullstack.__all__, invalidformatting.__all__, jutlexception.__all__, missinginput.__all__
