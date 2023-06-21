55aaee0bc8e9c8753629220475c8001cc467f369
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-06-21 15:39:05 -0700

    build: Added printlenth submodule.

--- jutl/utilities/__init__.py
+++ jutl/utilities/__init__.py
@@ -2,7 +2,8 @@
 # functions are usable at
 # 'from jutl import utilities' level.
 from .helloworld import *
+from .printlength import *
 
 # Only show functions specified in
 # submodule files to the outside world.
-__all__ = helloworld.__all__
+__all__ = helloworld.__all__, printlength.__all__
