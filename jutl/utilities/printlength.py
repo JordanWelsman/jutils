47fcc0b15fe37b9f58da00b18534acad178b244b
Jordan Welsman <jordan.welsman@hotmail.co.uk>
2023-06-21 15:54:31 -0700

    feat: Now supports *args.

--- jutl/utilities/printlength.py
+++ jutl/utilities/printlength.py
@@ -2,11 +2,11 @@
 __all__ = ['printlen']
 
 
-def printlen(item) -> int:
+def printlen(*items) -> int:
     """
     Prints the length of the passed item.
     Use this instead of `print(len(item))`.
     This is possibly the laziest function I've ever written.
     """
-    assert hasattr(item, '__len__')
-    print(len(item))
+    assert all(hasattr(item, '__len__') for item in items)
+    print(*[len(item) for item in items], sep=", ")
